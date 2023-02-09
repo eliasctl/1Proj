92dee897ca00fc6fe418a25a87b92ff3230169cc
Le_Vrai_WIWI <superwoup@gmail.com>
2023-02-09 22:59:48 +0100

    clear files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // On récupère le fichiers fonctions.fr
 const fonctions = require("./fonctions.js");
+
 function askUser() {
   let choice = prompt("Choisissez un nombre entre 5, 7, 9 ou 11");
   return choice;
