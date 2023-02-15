fe3a1be6079fd6acdeb4fe2cfe27cd62c5bbbda6
Le_Vrai_WIWI <superwoup@gmail.com>
2023-02-15 17:42:23 +0100

    modify test.py

--- test.py
+++ test.py
@@ -1,14 +1,29 @@
+# import des modules
 import pygame
-import random
+from fonctions import *
 
-# Initialisation de Pygame
-pygame.init()
 
-# Définition de la taille de la fenêtre
-window_size = (400, 300)
+# un joueur (se deplacer, poser des barrières)
+class Joueur:
+    def __init__(self, x, y, couleur):
+        self.x = x
+        self.y = y
+        self.couleur = couleur
 
-# Création de la fenêtre
-screen = pygame.display.set_mode(window_size)
+    def deplacer(self):
+        # on recupere les touches enfoncées
+        touches = pygame.key.get_pressed()
+
+    def pose_une_barrière(self, x, y):
+        bariere_x = x
+        bariere_y = y
+
+
+# pour le jeux
+pygame.init()  # Initialisation de Pygame
+window_size = (400, 300)  # Définition de la taille de la fenêtre
+screen = pygame.display.set_mode(window_size)  # Création de la fenêtre
+joueur = Joueur(0, 0, (255, 0, 0))  # on crée un joueur
 
 # Boucle principale du jeu
 running = True
@@ -17,18 +32,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.MOUSEBUTTONUP:
-            # Récupération de la position de la souris
-            mouse_pos = pygame.mouse.get_pos()
-            # Exécution d'une action si le clic a été effectué dans la zone définie
-            print("Clic détecté dans la zone définie")
 
-            # Mise à jour de l'écran
-            screen.fill((random.randint(0, 255),
-                         random.randint(0, 255),
-                         random.randint(0, 255)))
-    # Mise à jour de l'écran
-    pygame.display.update()
+    pygame.display.update()  # Mise à jour de l'écran
+pygame.quit()  # Nettoyage
 
-# Nettoyage
-pygame.quit()
+# TODO : Une grille sur le plateau de jeux
+# TODO : Le deplacement des joueurs
+# TODO : Le fait de poser des barrières
+# TODO : La fonction de victoire
+# TODO : L'affichage des joueurs sur le plateau
