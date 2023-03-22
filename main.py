6485dda45c21fdab92720db9e5f2c7a8bf920f85
Le_Vrai_WIWI <superwoup@gmail.com>
2023-03-22 16:23:20 +0100

    zizi

--- main.py
+++
@@ -1,4 +0,0 @@
-# Importation du fchier fonction qui contient tous ce qui est nécessaire au fonctionnement du jeu
-from fonctions import *
-
-nombre_de_joueurs = initialisation_du_nombre_de_joueurs()
