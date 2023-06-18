aad3af7e9414f4dbc5b20385236fc9dc0237424b
Paul Mareschi <117636519+Caalagan@users.noreply.github.com>
2023-06-18 12:55:40 +0200

    Commencement des commentaires (rectifications)

--- quoridor.py
+++ quoridor.py
@@ -1,18 +1,19 @@
+# Auteurs : William Bergue, Paul Mareschi, Elias Moussa-Osman, Clovis Kouoi
+
+# Importation des libraries
 import pygame
 import sys
 from func import *
 from pygame import mixer
-
-# Possibilité de relancer la partie
     
 
-# mettre en place la musique
+# Mise en place la musique
 mixer.init()
 mixer.music.load('Bloons TD battles music Monkeys and Bloons!.mp3')
 mixer.music.set_volume(1)
 mixer.music.play(-1)
 
-# classe joueur
+# Initialisation de la classe joueur
 class Joueur:
     def __init__(self, x, y, couleur, nb_murs):
         self.x = x
@@ -20,248 +21,340 @@ class Joueur:
         self.couleur = couleur
         self.nb_mur = nb_murs
 
+    # Affichage du joueur
     def affichage_joueur(self, fenetre, couleur, indx, indy):
         pygame.draw.circle(fenetre, couleur, (Quadrillage_dx+Qpas_x*(indx-0.5)-Quad_mur/2,
                                               Quadrillage_dy+Qpas_y*(indy-0.5)-Quad_mur/2), Qpas_x/3, 0)
 
+    # Déplacement du joueur
     def deplacer(self, fenetre, nb_joueur, taille_plateau):
         deplace_possible = []
 
-        # on check les murs
-        if self.x > 1:  # si le joueur n'est pas sur la premiere colonne mouv vers le gauche
+        # Vérification des murs
+        if self.x > 1:  # Si le joueur n'est pas sur la premiere colonne déplacement vers la gauche
             presence_mur = False
             if self.y > 1:
-                # print("valeur X = " + str(self.x) + " Valeur Y=" + str(self.y))
                 if tableauMurV[int(self.x-2)][int(self.y-2)] != 0:
                     presence_mur = True
             if self.y < taille_plateau:
                 if tableauMurV[int(self.x-2)][int(self.y-1)] != 0:
                     presence_mur = True
 
-            # on check les joueurs
+            # Vérification des joueurs
             if not presence_mur:
                 presence_joueur = False
+
                 for i in range(nb_joueur):
+
                     if self.x-1 == joueurs[i].x and self.y == joueurs[i].y:
                         presence_joueur = True
 
                 if not presence_joueur:
+
                     if tableauMurV[int(self.x-2)][int(self.y-1)] == 0:
                         self.affichage_joueur(fenetre, blanc, self.x-1, self.y)
                         deplace_possible.append((self.x-1, self.y))
+
                 elif self.x > 2:
                     presence_mur_case2 = False
+
                     if tableauMurV[int(self.x-2)][int(self.y)] == 0 and tableauMurV[int(self.x-2)][int(self.y-1)] == 0:
                         presence_mur_case2 = True
 
                     if not presence_mur_case2:
                         presence_joueur_case2 = False
+
                         for j in range(nb_joueur):
+
                             if self.x-2 == joueurs[j].x and self.y == joueurs[j].y:
                                 presence_joueur_case2 = True
+
                         if presence_joueur_case2 == False:
                             self.affichage_joueur(
                                 fenetre, blanc, self.x-2, self.y)
                             deplace_possible.append((self.x-2, self.y))
 
-        if self.x < taille_plateau:  # si le joueur n'est pas sur la derniere colonne mouv vers le la droite
+        # Vérification des murs
+        if self.x < taille_plateau:  # Si le joueur n'est pas sur la derniere colonne déplacement vers la droite
             presence_mur = False
             if self.y > 1:
+
                 if tableauMurV[int(self.x-1)][int(self.y-2)] != 0:
                     presence_mur = True
+
             if self.y < taille_plateau:
+
                 if tableauMurV[int(self.x-1)][int(self.y-1)] != 0:
                     presence_mur = True
-
+            # Vérification des joueurs
             if not presence_mur:
                 presence_joueur = False
+
                 for i in range(nb_joueur):
+
                     if self.x+1 == joueurs[i].x and self.y == joueurs[i].y:
                         presence_joueur = True
+
                 if not presence_joueur:
+
                     if tableauMurV[int(self.x-1)][int(self.y-1)] == 0:
                         self.affichage_joueur(fenetre, blanc, self.x+1, self.y)
                         deplace_possible.append((self.x+1, self.y))
+
                 elif self.x+2 < taille_plateau:
                     presence_mur_case2 = False
+
                     if tableauMurV[int(self.x+1)][int(self.y)] == 0 and tableauMurV[int(self.x+1)][int(self.y-1)] == 0:
                         presence_mur_case2 = True
 
                     if not presence_mur_case2:
                         presence_joueur_case2 = False
+
                         for i in range(nb_joueur):
+
                             if self.x+2 == joueurs[i].x and self.y == joueurs[i].y:
                                 presence_joueur_case2 = True
+
                         if presence_joueur_case2 == False:
                             self.affichage_joueur(
                                 fenetre, blanc, self.x+2, self.y)
                             deplace_possible.append((self.x+2, self.y))
 
-        if self.y > 1:  # verifie que le joueur n'est pas sur la premiere colonne pour dep vers haut
+        # Vérification des murs
+        if self.y > 1:  # Si le joueur n'est pas sur la derniere colonne déplacement vers le haut
             presence_mur = False
+
             if self.x > 1:
+
                 if tableauMurH[int(self.x-2)][int(self.y-2)] != 0:
                     presence_mur = True
+
             if self.x < taille_plateau:
+
                 if tableauMurH[int(self.x-1)][int(self.y-2)] != 0:
                     presence_mur = True
-
+            # Vérification des joueurs
             if not presence_mur:
                 presence_joueur = False
+
                 for i in range(nb_joueur):
+
                     if self.x == joueurs[i].x and self.y-1 == joueurs[i].y:
                         presence_joueur = True
+
                 if not presence_joueur:
                     self.affichage_joueur(fenetre, blanc, self.x, self.y-1)
                     deplace_possible.append((self.x, self.y-1))
+
                 elif self.y-2 > 0:
                     presence_mur_case2 = False
+
                     if tableauMurV[int(self.x)][int(self.y-2)] == 0 and tableauMurV[int(self.x-1)][int(self.y-2)] == 0:
                         presence_mur_case2 = True
 
                     if not presence_mur_case2:
                         presence_joueur_case2 = False
+
                         for i in range(nb_joueur):
+
                             if self.x == joueurs[i].x and self.y-2 == joueurs[i].y:
                                 presence_joueur_case2 = True
+
                         if presence_joueur_case2 == False:
                             self.affichage_joueur(
                                 fenetre, blanc, self.x, self.y-2)
                             deplace_possible.append((self.x, self.y-2))
 
-        if self.y < taille_plateau:  # verifie que le joueur n'est pas sur la derniere colonne pour dep vers bas
+        # Vérification des murs
+        if self.y < taille_plateau: # Si le joueur n'est pas sur la derniere colonne déplacement vers le bas
             presence_mur = False
+
             if self.x > 1:
+
                 if tableauMurH[int(self.x-2)][int(self.y-1)] != 0:
                     presence_mur = True
+
             if self.x < taille_plateau:
+
                 if tableauMurH[int(self.x-1)][int(self.y-1)] != 0:
                     presence_mur = True
 
+            # Vérification des joueurs
             if not presence_mur:
                 presence_joueur = False
+
                 for i in range(nb_joueur):
+
                     if self.x == joueurs[i].x and self.y+1 == joueurs[i].y:
                         presence_joueur = True
+
                 if not presence_joueur:
+
                     if tableauMurH[int(self.x-1)][int(self.y-1)] == 0:
                         self.affichage_joueur(fenetre, blanc, self.x, self.y+1)
                         deplace_possible.append((self.x, self.y+1))
+
                 elif self.y+2 < taille_plateau:
                     presence_mur_case2 = False
+
                     if tableauMurV[int(self.x)][int(self.y+1)] == 0 and tableauMurV[int(self.x-1)][int(self.y+1)] == 0:
                         presence_mur_case2 = True
 
                     if not presence_mur_case2:
                         presence_joueur_case2 = False
+
                         for i in range(nb_joueur):
+
                             if self.x == joueurs[i].x and self.y+2 == joueurs[i].y:
                                 presence_joueur_case2 = True
+
                         if presence_joueur_case2 == False:
                             self.affichage_joueur(
                                 fenetre, blanc, self.x, self.y+2)
                             deplace_possible.append((self.x, self.y+2))
         pygame.display.flip()
 
-        # on attend que le joueur clique sur une case
+        # Attente du clic du joueur
         while True:
             ev = pygame.event.poll()
-            # si on clique sur un bouton
+
+            # Evennement de clic
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
+
                 if pos[0] > Quadrillage_dx and pos[0] < Quadrillage_dx+Quadrillage_lX and pos[1] > Quadrillage_dy and pos[1] < Quadrillage_dy+Quadrillage_ly:
-                    # on recupere la case ou le joueur a clique
+                    # Récupération de la case cliquée
                     indx = int((pos[0]-Quadrillage_dx)/Qpas_x)+1
                     indy = int((pos[1]-Quadrillage_dy)/Qpas_y)+1
-                    # on vérifie que la case est dans le tableau
+
+                    # Vérification que la case cliquée est dans le tableau
                     if (indx, indy) in deplace_possible:
-                        # on deplace le joueur
+                        # Déplacement du joueur
                         self.x = indx
                         self.y = indy
                         break
 
+    # Fonction pour poser un mur vertical
     def poser_murV(self, fenetre, tableauMurV, taille_plateau):
-        # on init un tableau avec les coordonnées des murs possibles
+        # Initialisation d'un tableau avec les coordonnées des murs possibles
         mur_possible = [[False for i in range(
             taille_plateau-1)] for j in range(taille_plateau-1)]
 
-        # on affiche les milieux de murs possibles
+        # Affichage des milieux de murs possibles
         for i in range(taille_plateau-1):
+
             for j in range(taille_plateau-1):
+
                 if tableauMurV[i][j] == 0 and tableauMurH[i][j] == 0:
-                    if j > 0:  # si on est pas sur la premiere ligne
-                        if j < taille_plateau-2:  # si on est pas sur la derniere ligne
-                            # si il n'y a pas de mur au dessus et en dessous
+
+                    if j > 0:  # Si le murs n'est pas sur la premiere ligne
+
+                        if j < taille_plateau-2:  # Si le mur n'est pas sur la derniere ligne
+                            
+                            # Si il n'y a pas de mur au dessus et en dessous
                             if tableauMurV[i][j-1] == 0 and tableauMurV[i][j+1] == 0:
                                 pygame.draw.circle(fenetre, blanc, (Quadrillage_dx+Qpas_x*(
                                     i+1)-Quad_mur/2, Quadrillage_dy+Qpas_y*(j+1)-Quad_mur/2), 6, 0)
                                 mur_possible[i][j] = True
-                        else:  # si on est sur la derniere ligne
+
+                        else:  # Si on est sur la derniere ligne
+
                             if tableauMurV[i][j-1] == 0:
                                 pygame.draw.circle(fenetre, blanc, (Quadrillage_dx+Qpas_x*(
                                     i+1)-Quad_mur/2, Quadrillage_dy+Qpas_y*(j+1)-Quad_mur/2), 6, 0)
                                 mur_possible[i][j] = True
-                    else:  # si on est sur la premiere ligne
+
+                    else:  # Si on est sur la premiere ligne
+
                         if tableauMurV[i][j+1] == 0:
                             pygame.draw.circle(fenetre, blanc, (Quadrillage_dx+Qpas_x*(
                                 i+1)-Quad_mur/2, Quadrillage_dy+Qpas_y*(j+1)-Quad_mur/2), 6, 0)
                             mur_possible[i][j] = True
+
         pygame.display.flip()
-        mur_choisi = False  # on attend que le joueur clique sur un neoud de mur
+
+        mur_choisi = False  # Attente que le joueur clique sur un neoud de mur
+
         while mur_choisi == False:
             ev = pygame.event.poll()
-            if pygame.mouse.get_pressed()[0]:  # si on clique
+
+            # Evennement de clic
+            if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
+
                 if pos[0] > Quadrillage_dx and pos[0] < Quadrillage_dx+Quadrillage_lX and pos[1] > Quadrillage_dy and pos[1] < Quadrillage_dy+Quadrillage_ly:
-                    # on cherche la case ou le joueur a clique
+                    
+                    # Récupération de la case cliquée
                     for i in range(taille_plateau-1):
+
                         for j in range(taille_plateau-1):
+
                             if tableauMurV[i][j] == 0 and tableauMurH[i][j] == 0 and mur_possible[i][j] == True:
                                 distance = (Quadrillage_dx+Qpas_x*(i+1)-Quad_mur/2-pos[0])**2+(
                                     Quadrillage_dy+Qpas_y*(j+1)-Quad_mur/2-pos[1])**2
+                                
                                 if distance < 36:
-                                    # print("poser murV")
                                     tableauMurV[i][j] = self.couleur
                                     mur_choisi = True
                                     break
 
+    # Fonction pour poser un mur horizontal                                
     def poser_murH(self, fenetre, tableauMurH, taille_plateau):
-        # on init un tableau avec les coordonnées des murs possibles
+        # Initialisation d'un tableau avec les coordonnées des murs possibles
         mur_possible = [[False for i in range(
             taille_plateau-1)] for j in range(taille_plateau-1)]
 
-        # on affiche les milieux de murs possibles
+       # Affichage des milieux de murs possibles
         for i in range(taille_plateau-1):
+
             for j in range(taille_plateau-1):
+
                 if tableauMurH[i][j] == 0 and tableauMurV[i][j] == 0:
-                    if i > 0:
-                        if i < taille_plateau-2:
+
+                    if i > 0:  # Si le murs n'est pas sur la premiere colonne
+
+                        if i < taille_plateau-2: # Si le mur n'est pas sur la derniere colonne
+                                
+                            # Si il n'y a pas de mur à gauche et à droite
                             if tableauMurH[i-1][j] == 0 and tableauMurH[i+1][j] == 0:
                                 pygame.draw.circle(fenetre, blanc, (Quadrillage_dx+Qpas_x*(
                                     i+1)-Quad_mur/2, Quadrillage_dy+Qpas_y*(j+1)-Quad_mur/2), 6, 0)
                                 mur_possible[i][j] = True
-                        else:
+
+                        else: # Si on est sur la derniere colonne
+                             
                             if tableauMurH[i-1][j] == 0:
                                 pygame.draw.circle(fenetre, blanc, (Quadrillage_dx+Qpas_x*(
                                     i+1)-Quad_mur/2, Quadrillage_dy+Qpas_y*(j+1)-Quad_mur/2), 6, 0)
                                 mur_possible[i][j] = True
-                    else:
+                    else: # Si on est sur la premiere colonne
                         if tableauMurH[i+1][j] == 0:
                             pygame.draw.circle(fenetre, blanc, (Quadrillage_dx+Qpas_x*(
                                 i+1)-Quad_mur/2, Quadrillage_dy+Qpas_y*(j+1)-Quad_mur/2), 6, 0)
                             mur_possible[i][j] = True
+
         pygame.display.flip()
-        mur_choisi = False
+
+        mur_choisi = False # Attente que le joueur clique sur un neoud de mur
+
         while mur_choisi == False:
             ev = pygame.event.poll()
+
+            # Evennement de clic
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
+
                 if pos[0] > Quadrillage_dx and pos[0] < Quadrillage_dx+Quadrillage_lX and pos[1] > Quadrillage_dy and pos[1] < Quadrillage_dy+Quadrillage_ly:
+                    
+                    # Récupération de la case cliquée
                     for i in range(taille_plateau-1):
+
                         for j in range(taille_plateau-1):
+
                             if tableauMurH[i][j] == 0 and tableauMurV[i][j] == 0 and mur_possible[i][j] == True:
                                 distance = (Quadrillage_dx+Qpas_x*(i+1)-Quad_mur/2-pos[0])**2+(
                                     Quadrillage_dy+Qpas_y*(j+1)-Quad_mur/2-pos[1])**2
+                                
                                 if distance < 36:
                                     # print("poser murH")
                                     tableauMurH[i][j] = self.couleur
@@ -346,6 +439,7 @@ while not partie_finie:
                         fenetre_jeu, tableauMurH, taille_plateau)
                     pygame.display.flip()
                     break
+
         #test de victoire
         if victoire(joueurs[i].x, joueurs[i].y, taille_plateau, joueurs[i].couleur)==True:
 
