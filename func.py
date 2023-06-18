67c0f272723ba1b052a59fa37078dc669c6e0190
Paul Mareschi <117636519+Caalagan@users.noreply.github.com>
2023-06-18 13:58:38 +0200

    Fin des correctifs de comentaires

--- func.py
+++ func.py
@@ -1,3 +1,5 @@
+# Importation des libraries
+
 import pygame
 import time
 import os
@@ -5,7 +7,7 @@ import os
 # Initialisation de Pygame
 pygame.init()
 
-# couleur
+# Création des couleurs
 noir = (0, 0, 0)
 blanc = (255, 255, 255)
 bleu = (0, 0, 255)
@@ -15,11 +17,11 @@ violet = (84, 0, 201)
 gris = (150, 150, 150)
 ombre = (50, 50, 50)
 
-# variables du jeux
+# Création des variables de jeu
 nb_joueur = 0
 taille_plateau = 0
 
-# variables graphiques
+# Céation des variables garphiques
 Quadrillage_dx = 200  # position X du coin haut gauche du quadrillage
 Quadrillage_dy = 100  # position Y du coin haut gauche du quadrillage
 Quadrillage_lX = 400  # longueur du quadrillage
@@ -29,7 +31,7 @@ Qpas_y = 1  # pas du quadrillage en Y
 Quad_mur = 5  # largeur des murs du quadrillage
 
 
-# fonction qui affiche la page principale
+# Fonction qui affiche la page principale
 def cree_page_principale():
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Quoridor")  # titre de la fenetre
@@ -38,7 +40,8 @@ def cree_page_principale():
 
 # Création d'un bouton complet avec texte et ombre
 def creation_bouton(screen, x, y, hauteur, largeur, couleurBoutton, couleurText, text, PossedeUneOmbre):
-    # affichage ombre
+    
+    # Affichage ombre
     if PossedeUneOmbre:
         button_rect = pygame.Rect(x+3, y+3, largeur, hauteur)
         button_color = pygame.Color(ombre)
@@ -47,21 +50,21 @@ def creation_bouton(screen, x, y, hauteur, largeur, couleurBoutton, couleurText,
     # Affichage du bouton
     button_rect = pygame.Rect(x, y, largeur, hauteur)
     button_color = pygame.Color(couleurBoutton)
-    button_text = pygame.font.SysFont(None, 23).render(
-        text, True, pygame.Color(couleurText))
+    button_text = pygame.font.SysFont(None, 23).render(text, True, pygame.Color(couleurText))
     pygame.draw.rect(screen, button_color, button_rect)
     screen.blit(button_text, button_rect.move(largeur/4, hauteur/2))
 
 
-# fonction qui affiche du texte
+# Fonction d'affichage de texte
 def affiche_text(screen, x, y, couleurText, text):
     font = pygame.font.Font(None, 36)
     text = font.render(text, 1, couleurText)
     screen.blit(text, (x, y))
 
 
-# fonction qui affiche le menu du nombre de joueur
+# Affichage du menu pour le choix du nombre de joueur
 def afficher_menu_nb_joueur(fenetre):
+
     # Clear fenetre
     fenetre.fill(noir)
 
@@ -70,21 +73,25 @@ def afficher_menu_nb_joueur(fenetre):
     affiche_text(fenetre, 250, 200, blanc, "Choississez le nombre de joueurs")
 
 
-    # boutons pour choisir le nombre de joueur
+    # Création des boutons pour choisir le nombre de joueur
     creation_bouton(fenetre, 100, 300, 200, 200, bleu, blanc, "2 joueurs", True)
     creation_bouton(fenetre, 500, 300, 200, 200, bleu, blanc, "4 joueurs", True)
 
     pygame.display.flip()
 
+    # Attente du choix du nombre de joueur
     nb_joueur = 0
     while nb_joueur == 0:
-        # test de sortie
+
+        # Tests de sortie
         ev = pygame.event.poll()
         if ev.type == pygame.KEYDOWN and ev.key == pygame.K_ESCAPE:
             pygame.quit()
+
         if ev.type == pygame.QUIT:
             pygame.quit()
-        # si on clique sur un bouton
+
+        # Evennement de clic sur un bouton
         if pygame.mouse.get_pressed()[0]:
             pos = pygame.mouse.get_pos()
             if pos[0] > 100 and pos[0] < 300 and pos[1] > 300 and pos[1] < 500:
@@ -95,8 +102,9 @@ def afficher_menu_nb_joueur(fenetre):
     return nb_joueur
 
 
-# fonction qui affiche le menu de la taille du plateau
+# ffichage du menu pour le choix de la taille du plateau
 def afficher_menu_taille_plateau(fenetre):
+
     # Clear fenetre
     fenetre.fill(noir)
 
@@ -104,20 +112,27 @@ def afficher_menu_taille_plateau(fenetre):
     affiche_text(fenetre, 250, 100, blanc, "Bienvenue sur Quoridor")
     affiche_text(fenetre, 250, 200, blanc, "Choississez la taille du plateau")
 
-
-
-    # on dessine les 4 boutons
+    # Création des boutons pour choisir la taille du plateau
     creation_bouton(fenetre, 100, 300, 100, 100, gris, blanc, "5 x 5", True)
     creation_bouton(fenetre, 500, 300, 100, 100, gris, blanc, "7 x 7", True)
     creation_bouton(fenetre, 100, 450, 100, 100, gris, blanc, "9 x 9", True)
     creation_bouton(fenetre, 500, 450, 100, 100, gris, blanc, "11 x 11", True)
+
     time.sleep(0.1)
     pygame.display.flip()
 
+    # Attente du choix de la taille du plateau
     taille_plateau = 0
     while taille_plateau == 0:
+        # Tests de sortie
         ev = pygame.event.poll()
-        # si on clique sur un bouton
+        if ev.type == pygame.KEYDOWN and ev.key == pygame.K_ESCAPE:
+            pygame.quit()
+
+        if ev.type == pygame.QUIT:
+            pygame.quit()
+
+        # Evennement de clic sur un bouton
         if pygame.mouse.get_pressed()[0]:
             pos = pygame.mouse.get_pos()
             if pos[0] > 100 and pos[0] < 200 and pos[1] > 300 and pos[1] < 400:
@@ -128,10 +143,10 @@ def afficher_menu_taille_plateau(fenetre):
                 taille_plateau = 9
             if pos[0] > 500 and pos[0] < 600 and pos[1] > 450 and pos[1] < 550:
                 taille_plateau = 11
-    # pygame.quit()
+
     return taille_plateau
 
-# Fonction qui affiche la confirmation de choix
+# Affichage du menu de confirmation des choix
 def afficher_confirmation_choix(fenetre, nb_joueur, taille_plateau, mode_jeu):
     fenetre.fill(noir)
     affiche_text(fenetre, 250, 50, blanc, "Confirmation de vos choix : ")
@@ -144,8 +159,15 @@ def afficher_confirmation_choix(fenetre, nb_joueur, taille_plateau, mode_jeu):
 
     choix_fait = 0
     while choix_fait == 0:
+        # Tests de sortie
         ev = pygame.event.poll()
-        # si on clique sur un bouton
+        if ev.type == pygame.KEYDOWN and ev.key == pygame.K_ESCAPE:
+            pygame.quit()
+
+        if ev.type == pygame.QUIT:
+            pygame.quit()
+
+        # Evennement de clic sur un bouton
         if pygame.mouse.get_pressed()[0]:
             pos = pygame.mouse.get_pos()
             if pos[0] > 250 and pos[0] < 350 and pos[1] > 500 and pos[1] < 600:
@@ -156,8 +178,9 @@ def afficher_confirmation_choix(fenetre, nb_joueur, taille_plateau, mode_jeu):
     return choix_fait
 
 
-# Fonction qui affiche les choix pour le mode de jeu et retourne les choix sous forme de liste
+# Affichage des choix pour le mode de jeu et renvoie des choix sous forme de liste
 def afficher_menu_jeu(fenetre):
+
     # Création de la liste des choix
     liste_choix = []
 
@@ -188,14 +211,21 @@ def afficher_menu_jeu(fenetre):
     pygame.display.flip()
 
     
-
+    # Attente des choix
     fin = 0
     taille_plateau = 0
     nb_joueur = 0
     mode_jeu = 0
     while fin == 0:
+        # Tests de sortie
         ev = pygame.event.poll()
-        # si on clique sur un bouton
+        if ev.type == pygame.KEYDOWN and ev.key == pygame.K_ESCAPE:
+            pygame.quit()
+
+        if ev.type == pygame.QUIT:
+            pygame.quit()
+
+        # Evennement de clic sur un bouton
         if pygame.mouse.get_pressed()[0]:
             pos = pygame.mouse.get_pos()
             if pos[0] > 400 and pos[0] < 490 and pos[1] > 250 and pos[1] < 540:
@@ -220,7 +250,8 @@ def afficher_menu_jeu(fenetre):
             if pos[0] > 350 and pos[0] < 430 and pos[1] > 500 and pos[1] < 700:
                 if taille_plateau != 0 and nb_joueur != 0 and mode_jeu != 0:
                     fin = 1
-    # pygame.quit()
+
+    # Ajout des paramètres à la liste
     liste_choix.append(nb_joueur)
     liste_choix.append(taille_plateau)
     liste_choix.append(mode_jeu)
@@ -232,27 +263,27 @@ def afficher_menu_jeu(fenetre):
         afficher_menu_jeu(fenetre)
 
 
-# fonction qui affiche le plateau de jeu
+# Affichage du plateau de jeu
 def affichage_plateau(fenetre, nb_joueur, taille_plateau, joueur, joueur_actif, tableauMurH, tableauMurV):
+    
     # Clear fenetre
     fenetre.fill(noir)
 
-    # init du pas graphique
+    # Initialisation du pas graphique
     Qpas_x = Quadrillage_lX / taille_plateau
     Qpas_y = Quadrillage_ly / taille_plateau
 
     # Affichage des textes
     affiche_text(fenetre, 250, 30, joueur[joueur_actif].couleur,
                  "Joueur en cours : Joueur "+str(joueur_actif+1))
-    # affiche_text(fenetre, 250, 50, blanc, "tableau")
 
-    # affichage du quadrillage
+    # Affichage du quadrillage
     for i in range(taille_plateau):
         for j in range(taille_plateau):
             creation_bouton(fenetre, Quadrillage_dx+Qpas_x*i, Quadrillage_dy +
                             Qpas_y*j, Qpas_x-Quad_mur, Qpas_y-Quad_mur, gris, gris, "", False)
 
-    # affichage des murs
+    # Affichage des murs
     for i in range(taille_plateau-1):
         for j in range(taille_plateau-1):
             if tableauMurV[i][j] != 0:
@@ -262,7 +293,7 @@ def affichage_plateau(fenetre, nb_joueur, taille_plateau, joueur, joueur_actif, 
                 creation_bouton(fenetre, Quadrillage_dx+Qpas_x*i, Quadrillage_dy +
                                 Qpas_y*(j+1), Quad_mur, 2*Qpas_x, tableauMurH[i][j], tableauMurH[i][j], "", False)  # mur horizontal
 
-   # affichage des boutons (pour le deplacement ou le placement de mur)
+   # Affichage des boutons (pour le deplacement ou le placement de mur)
     creation_bouton(fenetre, 400, 510, 50, 150,
                     gris, blanc, "Se Deplacer", True)
 
@@ -271,9 +302,7 @@ def affichage_plateau(fenetre, nb_joueur, taille_plateau, joueur, joueur_actif, 
     creation_bouton(fenetre, 615, 300, 50, 165, gris,
                     blanc, "mur Horizontal", True)
 
-    # print("Tableau affiché")
-
-    # affichage des joueurs
+    # Affichage des joueurs
     joueur[0].affichage_joueur(fenetre, joueur[0].couleur,
                                joueur[0].x, joueur[0].y)
     joueur[1].affichage_joueur(fenetre, joueur[1].couleur,
@@ -283,7 +312,6 @@ def affichage_plateau(fenetre, nb_joueur, taille_plateau, joueur, joueur_actif, 
             fenetre, joueur[2].couleur, joueur[2].x, joueur[2].y)
         joueur[3].affichage_joueur(
             fenetre, joueur[3].couleur, joueur[3].x, joueur[3].y)
-    # print("Joueurs affichés")
 
     pygame.display.flip()
 
@@ -298,12 +326,12 @@ def initMurTabMur(taille_plateau):
     return mur
 
 
-# fonction qui affiche un tableau
+# Affichage du tableau
 def afficheTableau(tableau):
     for ligne in tableau:
         print(ligne)
 
-#Fonction de vérification de victoire
+# Vérification de victoire
 def victoire(x, y, taille_plateau, couleur):
     if couleur==rouge :
         if y==taille_plateau:
@@ -319,10 +347,11 @@ def victoire(x, y, taille_plateau, couleur):
             return True
         
 
-# fonction qui affiche la page de victoire
+# Affichage de la page de victoire
 def afficher_victoire(fenetre, joueur_actif, nb_coups, python, sys_argv):
+    
     # Clear fenetre
-    pygame.display.update(fenetre.fill(0))
+    fenetre.fill(noir)
 
     # Agrrandissement de la fenetre pour afficher le texte
     fenetre = pygame.display.set_mode((800, 700))
@@ -348,6 +377,8 @@ def afficher_victoire(fenetre, joueur_actif, nb_coups, python, sys_argv):
     creation_bouton(fenetre, 500, 400, 200, 200, bleu, blanc, "Quitter", True)
 
     pygame.display.flip()
+
+    # Attente du choix de l'utilisateur
     while True:
         # test de sortie
         ev = pygame.event.poll()
@@ -355,11 +386,13 @@ def afficher_victoire(fenetre, joueur_actif, nb_coups, python, sys_argv):
             pygame.quit()
         if ev.type == pygame.QUIT:
             pygame.quit()
-        # si on clique sur un bouton
+
+        # Evennement de clic sur un bouton
         if pygame.mouse.get_pressed()[0]:
             pos = pygame.mouse.get_pos()
             if pos[0] > 100 and pos[0] < 400 and pos[1] > 400 and pos[1] < 500:
-                # On relance le programme
+                
+                # Relance du programme pour une nouvelle partie
                 pygame.quit()
                 os.execv(python, [python] + sys_argv)
             if pos[0] > 500 and pos[0] < 700 and pos[1] > 400 and pos[1] < 500:
